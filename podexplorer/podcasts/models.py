8f4fe5c19cd13a73eab556eec49a584aeb1e97a6
gesparza3 <gesparza3@mail.csuchico.edu>
2019-04-15 17:31:41 -0700

    Starting over...

--- podexplorer/podcasts/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-class Podcast(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=300)
-    created_at = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(User, related_name="podcasts",
-                              on_delete=models.CASCADE, null=True)
